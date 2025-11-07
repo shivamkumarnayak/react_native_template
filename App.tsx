e8c70aebfdb5fc66860916b9880152d8e657a397
Shivam Kumar Nayak <snayak@datopic.com>
2025-11-07 16:06:11 +0530

    Adding theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { CustomTheme } from './app/utils/theme';
 
 export default function App() {
   return (
+    <PaperProvider theme={CustomTheme}>
     <View style={styles.container}>
       <Text>React Basic Template</Text>
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
   );
 }
 
